6b2f3e71caa9337ad908ef3dacc20832def9ec8e
Deyvi Villegas <deyvi132002@gmail.com>
2024-12-31 13:45:45 -0500

    header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const JetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -20,7 +23,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={JetbrainsMono.variable}>{children}</body>
+      <body className={JetbrainsMono.variable}>
+        <Header />
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
+      </body>
     </html>
   );
 }
