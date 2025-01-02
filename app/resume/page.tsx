f450ad1df178b838118bcc2df9d6ff0f07463bb9
Deyvi Villegas <deyvi132002@gmail.com>
2025-01-01 23:50:55 -0500

    resume-end

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -20,35 +20,35 @@ const about = {
     "Lorem ipsum dolor, sit amet consectetur adipisicing elit. At, ducimus. Sit laudantium non rerum nisi, itaque vero.",
   info: [
     {
-      fieldNaae: "Nombre",
+      fieldName: "Nombre",
       fieldValue: "Deyvi Villegas",
     },
     {
-      fieldNaae: "Número Telefónico",
+      fieldName: "Teléfono",
       fieldValue: "(+51) 906890881",
     },
     {
-      fieldNaae: "Experiencia",
+      fieldName: "Experiencia",
       fieldValue: "1 Año",
     },
     {
-      fieldNaae: "Skype",
+      fieldName: "Skype",
       fieldValue: "darkcode01",
     },
     {
-      fieldNaae: "Nacionalidad",
+      fieldName: "Nacionalidad",
       fieldValue: "Perú",
     },
     {
-      fieldNaae: "Email",
+      fieldName: "Email",
       fieldValue: "deyvi132002@gmail.com",
     },
     {
-      fieldNaae: "Freelance",
+      fieldName: "Freelance",
       fieldValue: "Disponible",
     },
     {
-      fieldNaae: "Lenguajes",
+      fieldName: "Lenguajes",
       fieldValue: "Español, Inglés",
     },
   ],
@@ -260,12 +260,60 @@ const Resume = () => {
 
             {/* Skills */}
             <TabsContent value="skills" className="w-full h-full">
-              skills
+              <div className="flex flex-col gap-[30px]">
+                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                  <h3 className="text-4xl font-bold">{skills.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                    {skills.description}
+                  </p>
+                </div>
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
+                  {skills.skillList.map((skill, index) => {
+                    return (
+                      <li key={index}>
+                        <TooltipProvider delayDuration={100}>
+                          <Tooltip>
+                            <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                              <div className="text-6xl group-hover:text-accent transition-all duration-300">
+                                {skill.icon}
+                              </div>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              <p className="capitalize">{skill.name}</p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </TabsContent>
 
             {/* About */}
-            <TabsContent value="about" className="w-full">
-              about
+            <TabsContent
+              value="about"
+              className="w-full text-center xl:text-left"
+            >
+              <div className="flex flex-col gap-[30px]">
+                <h3 className="text-4xl font-bold">{about.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {about.description}
+                </p>
+                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+                  {about.info.map((item, index) => {
+                    return (
+                      <li
+                        key={index}
+                        className="flex items-center justify-center xl:justify-start gap-4"
+                      >
+                        <span className="text-white/60">{item.fieldName}</span>
+                        <span className="text-xl">{item.fieldValue}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </TabsContent>
           </div>
         </Tabs>
